6376e67d60c954701aab24957374598c885b6778
Maxim Lyahovskij <asmaximax256@gmail.com>
2023-09-10 22:41:07 +0600

    training works, but stucks at 0.29 cost

--- main.cpp
+++ main.cpp
@@ -8,10 +8,6 @@
 #include <opencv4/opencv2/highgui.hpp>
 #include <opencv4/opencv2/imgproc.hpp>
 
-#include <random>
-std::default_random_engine generator;
-std::uniform_real_distribution<double> distribution(0.0, 1.0);
-
 #include "include/project_path.h"
 
 using std::cout;
@@ -20,6 +16,8 @@ using std::endl;
 // Data set image size: 28x28 pixels, total 784
 // sigmoid 1/(1+e^(-x))
 // Eigen matrix A(row, col)
+// training set label file: 32 bit 2049 (magic number), 32 bit 60000 (number of items), 8 bit label...
+// training set image file: 32 bit 2051 (magic number), 32 bit 60000 (number of items), 32 bit 28 (number of rows), 32 bit 28 (number of cols), 8 bit pixel...
 
 const cv::Size size_raw(28, 28);   // original one number
 const cv::Size size_big(250, 250); // upscaled one number
@@ -28,24 +26,39 @@ const cv::Size size_big(250, 250); // upscaled one number
 // const cv::Size size_raw(28 * 3, 28 * 1);   // original matrix of numbers
 // const cv::Size size_big(100 * 3, 100 * 1); // upscaled matrix of numbers
 
-typedef Eigen::Matrix<float, 10, 1> Vec10f;
+const uint8_t num_neurons = 20;
+
+typedef Eigen::Matrix<float, 10, 1> Vector;
 
 Eigen::Matrix<float, 28 * 28, 1> a1;  // input layer
-Vec10f a2;                            // hidden layer
-Vec10f a3;                            // output layer
-Vec10f z2;                            //
-Vec10f z3;                            //
+Vector a2;                            // hidden layer
+Vector a3;                            // output layer
+Vector z2;                            //
+Vector z3;                            //
 Eigen::Matrix<float, 10, 28 * 28> w2; // 2 layer weight
 Eigen::Matrix<float, 10, 10> w3;      // 3 layer weight
-Vec10f b2;                            // 2 layer bias
-Vec10f b3;                            // 3 layer bias
-Vec10f be2;                           // 2 layer error
-Vec10f be3;                           // 3 layer error
+Vector b2;                            // 2 layer bias
+Vector b3;                            // 3 layer bias
+Vector be2;                           // 2 layer error
+Vector be3;                           // 3 layer error
 
 Eigen::Matrix<float, 10, 28 * 28> dC_dw2; //
 Eigen::Matrix<float, 10, 10> dC_dw3;      //
-Vec10f dC_db2;                            //
-Vec10f dC_db3;                            //
+Vector dC_db2;                            //
+Vector dC_db3;                            //
+
+Eigen::Matrix<float, 10, 28 * 28> dC_dw2_avr; //
+Eigen::Matrix<float, 10, 10> dC_dw3_avr;      //
+Vector dC_db2_avr;                            //
+Vector dC_db3_avr;                            //
+
+size_t image_counter = 0;
+const uint32_t number_of_images = 60000;
+uint8_t label = 0;
+
+float mu = 10;
+uint16_t epoch = 0;
+float C_train = 0;
 
 void getImage(char* file_array, size_t image_number, cv::Mat& image)
 {
@@ -57,7 +70,7 @@ void getLabel(char* file_array, size_t image_number, uint8_t& label)
   memcpy(&label, file_array + 2 * sizeof(uint32_t) + image_number, 1);
 }
 
-uint8_t getAnswer(Vec10f& nn_out)
+uint8_t getAnswer(Vector& nn_out)
 {
   uint8_t answer = 0;
 
@@ -99,9 +112,9 @@ float sigmoid(float x)
   return 1.0 / (1 + exp(-x));
 }
 
-Vec10f sigmoid(Vec10f& in)
+Vector sigmoid(Vector& in)
 {
-  Vec10f out;
+  Vector out;
 
   for (uint16_t i = 0; i < 10; i++)
   {
@@ -111,50 +124,39 @@ Vec10f sigmoid(Vec10f& in)
   return out;
 }
 
-int main(int, char**)
+void train(char* train_images_bytes, char* train_labels_bytes)
 {
-  cout << "Hello, world!" << endl;
+  epoch++;
+  cout << "Start training, epoch: " << epoch << endl;
 
-  std::ifstream dataset_train_images(PROJECT_PATH + std::string("/dataset/train-images-idx3-ubyte/train-images.idx3-ubyte"), std::ios::binary);
-  std::ifstream dataset_train_labels(PROJECT_PATH + std::string("/dataset/train-labels-idx1-ubyte/train-labels.idx1-ubyte"), std::ios::binary);
-  std::vector<char> images_bytes((std::istreambuf_iterator<char>(dataset_train_images)), std::istreambuf_iterator<char>());
-  std::vector<char> labels_bytes((std::istreambuf_iterator<char>(dataset_train_labels)), std::istreambuf_iterator<char>());
+  C_train = 0;
+  image_counter = 0;
 
-  size_t image_counter = 0;
+  dC_dw2_avr.setZero();
+  dC_dw3_avr.setZero();
+  dC_db2_avr.setZero();
+  dC_db3_avr.setZero();
 
-  cv::Mat image(size_raw, CV_8UC1, cv::Scalar(0));
-  cv::Mat image_big;
-
-  w2.setRandom();
-  w3.setRandom();
-
-  uint8_t label = 0;
-
-  getImage(images_bytes.data(), image_counter, image);
-  getLabel(labels_bytes.data(), image_counter, label);
-  getInput(images_bytes.data(), image_counter, a1);
-
-  float mu = 10;
-
-  while (1)
+  while (image_counter < number_of_images)
   {
+    getLabel(train_labels_bytes, image_counter, label);
+    getInput(train_images_bytes, image_counter, a1);
+
     z2 = w2 * a1 + b2;
     a2 = sigmoid(z2);
     z3 = w3 * a2 + b3;
     a3 = sigmoid(z3);
 
-    Vec10f y = getDesiredOutput(label);
+    Vector y = getDesiredOutput(label);
 
     float C = 1.0 / 2.0 * (y - a3).transpose() * (y - a3);
 
-    cout << "y: " << y.transpose() << endl;
-    cout << "a3: " << a3.transpose() << endl;
-    cout << "C: " << C << endl;
+    C_train += C;
 
-    be3 = (y - a3).cwiseProduct(sigmoid(z3).cwiseProduct((Vec10f::Ones() - sigmoid(z3))));
-    be2 = (w3.transpose() * be3).cwiseProduct(sigmoid(z3).cwiseProduct((Vec10f::Ones() - sigmoid(z3))));
-    // cout << "be3: " << endl << be3 << endl;
-    // cout << "be2: " << endl << be2 << endl;
+    // cout << "C: " << C << endl;
+
+    be3 = (y - a3).cwiseProduct(sigmoid(z3).cwiseProduct((Vector::Ones() - sigmoid(z3))));
+    be2 = (w3.transpose() * be3).cwiseProduct(sigmoid(z3).cwiseProduct((Vector::Ones() - sigmoid(z3))));
 
     for (uint16_t j = 0; j < 10; j++)
     {
@@ -176,11 +178,186 @@ int main(int, char**)
       }
     }
 
-    w2 = w2 + mu * dC_dw2;
-    w3 = w3 + mu * dC_dw3;
+    dC_dw2_avr += dC_dw2;
+    dC_dw3_avr += dC_dw3;
+    dC_db2_avr += dC_db2;
+    dC_db3_avr += dC_db3;
 
-    b2 = b2 + mu * dC_db2;
-    b3 = b3 + mu * dC_db3;
+    image_counter++;
+
+    // cout << "image: " << image_counter << endl;
+  }
+
+  dC_dw2 = dC_dw2_avr / (float)number_of_images;
+  dC_dw3 = dC_dw3_avr / (float)number_of_images;
+  dC_db2 = dC_db2_avr / (float)number_of_images;
+  dC_db3 = dC_db3_avr / (float)number_of_images;
+
+  C_train = C_train / (float)number_of_images;
+
+  w2 = w2 + mu * dC_dw2;
+  w3 = w3 + mu * dC_dw3;
+  b2 = b2 + mu * dC_db2;
+  b3 = b3 + mu * dC_db3;
+
+  // w2 = w2 - mu * dC_dw2;
+  // w3 = w3 - mu * dC_dw3;
+  // b2 = b2 - mu * dC_db2;
+  // b3 = b3 - mu * dC_db3;
+
+  cout << "Epoch " << epoch << " training ended, C avr: " << C_train << endl;
+}
+
+void saveWeights()
+{
+  // w2.setRandom();
+  // w3.setRandom();
+  // b2.setRandom();
+  // b3.setRandom();
+  // cout << "w2: " << w2 << endl;
+  // cout << "w3: " << w3 << endl;
+  // cout << "b2: " << b2 << endl;
+  // cout << "b3: " << b3 << endl;
+
+  uint8_t* byte_array_w2 = (uint8_t*)w2.data();
+  uint8_t* byte_array_w3 = (uint8_t*)w3.data();
+  uint8_t* byte_array_b2 = (uint8_t*)b2.data();
+  uint8_t* byte_array_b3 = (uint8_t*)b3.data();
+
+  std::ofstream file_w2("w2.bin", std::ios::binary);
+  if (!file_w2)
+  {
+    cout << "Failed to open file for writing." << endl;
+    return;
+  }
+  file_w2.write(reinterpret_cast<const char*>(byte_array_w2), sizeof(w2));
+  file_w2.close();
+
+  std::ofstream file_w3("w3.bin", std::ios::binary);
+  if (!file_w3)
+  {
+    cout << "Failed to open file for writing." << endl;
+    return;
+  }
+  file_w3.write(reinterpret_cast<const char*>(byte_array_w3), sizeof(w3));
+  file_w3.close();
+
+  std::ofstream file_b2("b2.bin", std::ios::binary);
+  if (!file_b2)
+  {
+    cout << "Failed to open file for writing." << endl;
+    return;
+  }
+  file_b2.write(reinterpret_cast<const char*>(byte_array_b2), sizeof(b2));
+  file_b2.close();
+
+  std::ofstream file_b3("b3.bin", std::ios::binary);
+  if (!file_b3)
+  {
+    cout << "Failed to open file for writing." << endl;
+    return;
+  }
+  file_b3.write(reinterpret_cast<const char*>(byte_array_b3), sizeof(b3));
+  file_b3.close();
+}
+
+void loadWeights()
+{
+  unsigned char read_bytes_w2[sizeof(w2)];
+  unsigned char read_bytes_w3[sizeof(w3)];
+  unsigned char read_bytes_b2[sizeof(b2)];
+  unsigned char read_bytes_b3[sizeof(b3)];
+
+  std::ifstream file_w2("w2.bin", std::ios::binary);
+  if (!file_w2)
+  {
+    cout << "Failed to open file for reading." << endl;
+    return;
+  }
+  file_w2.read(reinterpret_cast<char*>(read_bytes_w2), sizeof(w2));
+  file_w2.close();
+  memcpy(w2.data(), read_bytes_w2, sizeof(w2));
+
+  std::ifstream file_w3("w3.bin", std::ios::binary);
+  if (!file_w3)
+  {
+    cout << "Failed to open file for reading." << endl;
+    return;
+  }
+  file_w3.read(reinterpret_cast<char*>(read_bytes_w3), sizeof(w3));
+  file_w3.close();
+  memcpy(w3.data(), read_bytes_w3, sizeof(w3));
+
+  std::ifstream file_b2("b2.bin", std::ios::binary);
+  if (!file_b2)
+  {
+    cout << "Failed to open file for reading." << endl;
+    return;
+  }
+  file_b2.read(reinterpret_cast<char*>(read_bytes_b2), sizeof(b2));
+  file_b2.close();
+  memcpy(b2.data(), read_bytes_b2, sizeof(b2));
+
+  std::ifstream file_b3("b3.bin", std::ios::binary);
+  if (!file_b3)
+  {
+    cout << "Failed to open file for reading." << endl;
+    return;
+  }
+  file_b3.read(reinterpret_cast<char*>(read_bytes_b3), sizeof(b3));
+  file_b3.close();
+  memcpy(b3.data(), read_bytes_b3, sizeof(b3));
+
+  // cout << "Read w2 : " << w2 << endl;
+  // cout << "Read w3 : " << w3 << endl;
+  // cout << "Read b2 : " << b2 << endl;
+  // cout << "Read b3 : " << b3 << endl;
+}
+
+int main(int, char**)
+{
+  cout << "Hello, world!" << endl;
+
+  cv::Mat hello(cv::Size(100, 100), CV_8UC1, cv::Scalar(255));
+  cv::imshow("hello", hello);
+  cv::waitKey();
+
+  std::ifstream dataset_train_images(PROJECT_PATH + std::string("/dataset/train-images-idx3-ubyte/train-images.idx3-ubyte"), std::ios::binary);
+  std::ifstream dataset_train_labels(PROJECT_PATH + std::string("/dataset/train-labels-idx1-ubyte/train-labels.idx1-ubyte"), std::ios::binary);
+  std::vector<char> train_images_bytes((std::istreambuf_iterator<char>(dataset_train_images)), std::istreambuf_iterator<char>());
+  std::vector<char> train_labels_bytes((std::istreambuf_iterator<char>(dataset_train_labels)), std::istreambuf_iterator<char>());
+
+  cv::Mat image(size_raw, CV_8UC1, cv::Scalar(0));
+  cv::Mat image_big;
+
+  w2.setRandom();
+  w3.setRandom();
+
+  getImage(train_images_bytes.data(), image_counter, image);
+  getLabel(train_labels_bytes.data(), image_counter, label);
+  getInput(train_images_bytes.data(), image_counter, a1);
+
+  loadWeights();
+
+  for (uint16_t i = 0; i < 1000; i++)
+  {
+    train(train_images_bytes.data(), train_labels_bytes.data());
+  }
+
+  saveWeights();
+
+  image_counter = 0;
+
+  while (1)
+  {
+    getImage(train_images_bytes.data(), image_counter, image);
+    getLabel(train_labels_bytes.data(), image_counter, label);
+    getInput(train_images_bytes.data(), image_counter, a1);
+
+    z2 = w2 * a1 + b2;
+    a2 = sigmoid(z2);
+    z3 = w3 * a2 + b3;
+    a3 = sigmoid(z3);
 
     int answer = getAnswer(a3);
 
@@ -193,23 +370,21 @@ int main(int, char**)
 
     cv::imshow("Image", image_big);
     char c = cv::waitKey();
-    // cout << (int)c << endl;
 
-    // left arrow - 83
+    // cout << (int)c << endl;
 
     if (c == 83)
     {
-      getImage(images_bytes.data(), image_counter, image);
-      getLabel(labels_bytes.data(), image_counter, label);
-      getInput(images_bytes.data(), image_counter, a1);
+      getImage(train_images_bytes.data(), image_counter, image);
+      getLabel(train_labels_bytes.data(), image_counter, label);
+      getInput(train_images_bytes.data(), image_counter, a1);
+      image_counter++;
     }
 
     if (c == 27)
     {
       break;
     }
-
-    image_counter++;
   }
 
   dataset_train_images.close();
